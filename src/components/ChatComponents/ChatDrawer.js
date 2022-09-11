ec3afba4a4b4641cdf3b831d2687170e71545dcf
Samim Middey <samimmiddey@gmail.com>
2022-09-11 08:59:29 +0530

    third commit - fixed chat drawer width issue

--- src/components/ChatComponents/ChatDrawer.js
+++ src/components/ChatComponents/ChatDrawer.js
@@ -28,79 +28,81 @@ const ChatDarwer = () => {
                }
             }}
          >
-            <Box
-               sx={{
-                  padding: '90px 1rem 1rem 1rem',
-                  borderBottom: darkMode === 'dark' ? '1px solid rgba(255, 255, 255, 0.12)' : '1px solid rgba(0, 0, 0, 0.12)'
-               }}
-            >
-               <CustomizedTextfield
-                  size='small'
-                  label='Search Contacts'
-                  width='100%'
-                  handleChange={handleChange}
-               />
-            </Box>
-            <Box
-               sx={{
-                  padding: '1rem 8px',
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'flex-start',
-                  justifyContent: 'flex-start',
-                  rowGap: '8px',
-                  overflowY: 'hidden',
-                  '&:hover': {
-                     overflowY: 'auto'
-                  }
-               }}
-            >
-               {chatDrawer.map((item, index) => (
-                  <ListItemButton
-                     key={index}
-                     disableRipple
-                     onClick={() => setCurrentChatIndex(index)}
-                     sx={{
-                        borderRadius: '8px',
-                        px: 2.5,
-                        py: 1.75,
-                        maxHeight: 70,
-                        transition: '300ms ease',
-                        display: 'flex',
-                        alignItems: 'center',
-                        columnGap: '1rem',
-                        width: '100%',
-                        backgroundColor: index === currentChatIndex ? `${currentColor}25` : '',
-                        '&:hover': {
-                           backgroundColor: index === currentChatIndex ? `${currentColor}25` : ''
-                        }
-                     }}
-                  >
-                     <Avatar src={item.img} />
-                     <Box>
-                        <Typography
-                           sx={{
-                              color: index === currentChatIndex && darkMode === 'dark' ? 'white' : 'text.primary',
-                              fontSize: '15px',
-                              fontWeight: 500
-                           }}
-                        >
-                           {item.name}
-                        </Typography>
-                        <Typography
-                           sx={{
-                              color: index === currentChatIndex && darkMode === 'dark' ? 'white' : 'text.secondary',
-                              fontSize: '13px'
-                           }}
-                        >
-                           {item.position}
-                        </Typography>
-                     </Box>
-                  </ListItemButton>
-               ))}
+            <Box sx={{ width: '256px' }}>
+               <Box
+                  sx={{
+                     padding: '90px 1rem 1rem 1rem',
+                     borderBottom: darkMode === 'dark' ? '1px solid rgba(255, 255, 255, 0.12)' : '1px solid rgba(0, 0, 0, 0.12)'
+                  }}
+               >
+                  <CustomizedTextfield
+                     size='small'
+                     label='Search Contacts'
+                     width='100%'
+                     handleChange={handleChange}
+                  />
+               </Box>
+               <Box
+                  sx={{
+                     padding: '1rem 8px',
+                     display: 'flex',
+                     flexDirection: 'column',
+                     alignItems: 'flex-start',
+                     justifyContent: 'flex-start',
+                     rowGap: '8px',
+                     overflowY: 'hidden',
+                     '&:hover': {
+                        overflowY: 'auto'
+                     }
+                  }}
+               >
+                  {chatDrawer.map((item, index) => (
+                     <ListItemButton
+                        key={index}
+                        disableRipple
+                        onClick={() => setCurrentChatIndex(index)}
+                        sx={{
+                           borderRadius: '8px',
+                           px: 2.5,
+                           py: 1.75,
+                           maxHeight: 70,
+                           transition: '300ms ease',
+                           display: 'flex',
+                           alignItems: 'center',
+                           columnGap: '1rem',
+                           width: '100%',
+                           backgroundColor: index === currentChatIndex ? `${currentColor}25` : '',
+                           '&:hover': {
+                              backgroundColor: index === currentChatIndex ? `${currentColor}25` : ''
+                           }
+                        }}
+                     >
+                        <Avatar src={item.img} />
+                        <Box>
+                           <Typography
+                              sx={{
+                                 color: index === currentChatIndex && darkMode === 'dark' ? 'white' : 'text.primary',
+                                 fontSize: '15px',
+                                 fontWeight: 500
+                              }}
+                           >
+                              {item.name}
+                           </Typography>
+                           <Typography
+                              sx={{
+                                 color: index === currentChatIndex && darkMode === 'dark' ? 'white' : 'text.secondary',
+                                 fontSize: '13px'
+                              }}
+                           >
+                              {item.position}
+                           </Typography>
+                        </Box>
+                     </ListItemButton>
+                  ))}
+               </Box>
             </Box>
          </Drawer>
-      </Box>
+      </Box >
    );
 }
 
